8899da0918eedf7c9a863cba3b8ea9fcdea2ba92
Nicholas Landau <nrlandau@gmail.com>
2018-10-10 12:46:37 -0400

    Created main program functionality

--- Program.cs
+++ Program.cs
@@ -6,9 +6,22 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string test = System.Console.ReadLine();
-            System.Console.WriteLine(test);
+            int number = 0;
+            while (number != -1)
+            {
+                System.Console.WriteLine("Input a positive number or -1 to quit:");
+                number = int.Parse(System.Console.ReadLine());
+                if(number < 1)
+                {
+                    continue;
+                }
+                System.Console.WriteLine("{0}\t\t{1}\t\t{2}","Number","Squared","Cubed");
+                System.Console.WriteLine("{0}\t\t{0}\t\t{0}","=======");
+                for(int i =1; i <= number; i++)
+                {
+                    System.Console.WriteLine("{0}\t\t{1}\t\t{2}",i, i * i, i * i * i);
+                }
+            }
         }
     }
 }
